9f112c3abdb2e05464f306ca57fc64121ef1a145
Bryhilevych Orest <91126054+OrestBruhylevych@users.noreply.github.com>
2022-04-28 20:06:27 +0300

    add 1 and 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+var throttle = require('lodash.throttle');
 
+
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    inputEmail: document.querySelector('.feedback-form label input'),
+    textarea: document.querySelector('.feedback-form label textarea')
+}
+
+populateForm();
+
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextInput, 500));
+
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+}
+
+function onTextInput(e) {
+    const name = e.target.name;
+    const massage = e.target.value;
+
+    formData[name] = massage;
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+    const savedMassage = localStorage.getItem(STORAGE_KEY);
+
+    if (savedMassage) {
+        const { email, message } = JSON.parse(savedMassage);
+
+        refs.inputEmail.value = email;
+        refs.textarea.value = message;
+    }
+}
